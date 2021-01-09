ed3a458631ca5d2503b7b0589fb6b9f52c2b1984
jacki0 <jackio@tuta.io>
2021-01-09 23:29:21 +0300

    create a struct

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome!")
 }
 
+type url struct {
+	Orig_URL    string `json:"orig_url"`
+	Reduced_URL string `json:"reduced_url"`
+	Date        string `json:"date"`
+}
+
+type urls []url
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
