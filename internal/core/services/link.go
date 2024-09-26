58d0f1e671a95f5ea6d27505675277fe6b63abad
pchchv <ipchchv@gmail.com>
2024-09-26 10:54:05 +0300

    Implement Create and Delete methods

--- internal/core/services/link.go
+++ internal/core/services/link.go
@@ -31,3 +31,17 @@ func (service *LinkService) GetOriginalURL(ctx context.Context, shortLinkKey str
 	}
 	return &data.OriginalURL, nil
 }
+
+func (service *LinkService) Create(ctx context.Context, link domain.Link) error {
+	if err := service.port.Create(ctx, link); err != nil {
+		return fmt.Errorf("failed to create short URL: %w", err)
+	}
+	return nil
+}
+
+func (service *LinkService) Delete(ctx context.Context, short string) error {
+	if err := service.port.Delete(ctx, short); err != nil {
+		return fmt.Errorf("failed to delete short URL for identifier '%s': %w", short, err)
+	}
+	return nil
+}
