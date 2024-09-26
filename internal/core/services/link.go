e9eac1dd0c9443eca3a7e1719af12da778f60c3b
pchchv <ipchchv@gmail.com>
2024-09-26 10:49:07 +0300

    Implement a NewLinkService

--- internal/core/services/link.go
+++ internal/core/services/link.go
@@ -6,3 +6,7 @@ type LinkService struct {
 	port  ports.LinkPort
 	cache ports.Cache
 }
+
+func NewLinkService(p ports.LinkPort, c ports.Cache) *LinkService {
+	return &LinkService{port: p, cache: c}
+}
