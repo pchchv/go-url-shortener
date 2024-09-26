041ab98e9862b3b4c342df21607e050e1cfa06fa
pchchv <ipchchv@gmail.com>
2024-09-26 10:56:55 +0300

    Implement an All and a Get methods

--- internal/core/services/stats.go
+++ internal/core/services/stats.go
@@ -1,6 +1,12 @@
 package services
 
-import "github.com/pchchv/go-url-shortener/internal/core/ports"
+import (
+	"context"
+	"fmt"
+
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
+	"github.com/pchchv/go-url-shortener/internal/core/ports"
+)
 
 type StatsService struct {
 	port  ports.StatsPort
@@ -9,4 +15,18 @@ type StatsService struct {
 
 func NewStatsService(p ports.StatsPort, c ports.Cache) *StatsService {
 	return &StatsService{port: p, cache: c}
+}
+
+func (service *StatsService) All(ctx context.Context) (stats []domain.Stats, err error) {
+	if stats, err = service.port.All(ctx); err != nil {
+		return nil, fmt.Errorf("failed to get all stats: %w", err)
+	}
+	return stats, nil
+}
+
+func (service *StatsService) Get(ctx context.Context, statsID string) (stats domain.Stats, err error) {
+	if stats, err = service.port.Get(ctx, statsID); err != nil {
+		return domain.Stats{}, fmt.Errorf("failed to get stats for identifier '%s': %w", statsID, err)
+	}
+	return stats, nil
 }
