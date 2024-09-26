86177948651fc93a3d301a0e332d62110add1fd8
pchchv <ipchchv@gmail.com>
2024-09-26 11:05:45 +0300

    Implement a Set and a Get methods

--- internal/tests/mock/mock_cache.go
+++ internal/tests/mock/mock_cache.go
@@ -18,6 +18,27 @@ func NewMockRedisCache() *MockRedisCache {
 	}
 }
 
+func (m *MockRedisCache) Set(ctx context.Context, key string, val string) error {
+	m.Store[key] = val
+	m.TTL[key] = time.Now().Add(time.Minute)
+	return nil
+}
+
+func (m *MockRedisCache) Get(ctx context.Context, key string) (string, error) {
+	val, ok := m.Store[key]
+	if !ok {
+		return "", errors.New("key not found")
+	}
+
+	if time.Now().After(m.TTL[key]) {
+		delete(m.Store, key)
+		delete(m.TTL, key)
+		return "", errors.New("key expired")
+	}
+
+	return val, nil
+}
+
 func (m *MockRedisCache) Delete(ctx context.Context, key string) error {
 	if _, ok := m.Store[key]; !ok {
 		return errors.New("key not found")
