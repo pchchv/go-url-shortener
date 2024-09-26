44d13b56838830dac9cf7e83057f63dada2aa6b7
pchchv <ipchchv@gmail.com>
2024-09-26 11:22:48 +0300

    Implement an All method

--- internal/adapters/adapters/repository/link.go
+++ internal/adapters/adapters/repository/link.go
@@ -2,10 +2,14 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
 )
 
 type LinkRepository struct {
@@ -24,4 +28,23 @@ func NewLinkRepository(ctx context.Context, tableName string) *LinkRepository {
 		client:    client,
 		tableName: tableName,
 	}
+}
+
+func (d *LinkRepository) All(ctx context.Context) ([]domain.Link, error) {
+	var links []domain.Link
+	input := &dynamodb.ScanInput{
+		TableName: &d.tableName,
+		Limit:     aws.Int32(20),
+	}
+
+	result, err := d.client.Scan(ctx, input)
+	if err != nil {
+		return links, fmt.Errorf("failed to get items from DynamoDB: %w", err)
+	}
+
+	if err = attributevalue.UnmarshalListOfMaps(result.Items, &links); err != nil {
+		return links, fmt.Errorf("failed to unmarshal data from DynamoDB: %w", err)
+	}
+
+	return links, nil
 }
