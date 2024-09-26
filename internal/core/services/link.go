a2ed99f9f14a988eab5557f84dda3e38bec08d44
pchchv <ipchchv@gmail.com>
2024-09-26 10:53:16 +0300

    Implement GetAll and GetOriginalURL methods

--- internal/core/services/link.go
+++ internal/core/services/link.go
@@ -1,6 +1,12 @@
 package services
 
-import "github.com/pchchv/go-url-shortener/internal/core/ports"
+import (
+	"context"
+	"fmt"
+
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
+	"github.com/pchchv/go-url-shortener/internal/core/ports"
+)
 
 type LinkService struct {
 	port  ports.LinkPort
@@ -9,4 +15,19 @@ type LinkService struct {
 
 func NewLinkService(p ports.LinkPort, c ports.Cache) *LinkService {
 	return &LinkService{port: p, cache: c}
+}
+
+func (service *LinkService) GetAll(ctx context.Context) (links []domain.Link, err error) {
+	if links, err = service.port.All(ctx); err != nil {
+		return nil, fmt.Errorf("failed to get all links: %w", err)
+	}
+	return links, nil
+}
+
+func (service *LinkService) GetOriginalURL(ctx context.Context, shortLinkKey string) (*string, error) {
+	data, err := service.port.Get(ctx, shortLinkKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get short URL for identifier '%s': %w", shortLinkKey, err)
+	}
+	return &data.OriginalURL, nil
 }
