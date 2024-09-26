362052ad92d022435a819ee1aab485416cc2fa1b
pchchv <ipchchv@gmail.com>
2024-09-26 11:07:24 +0300

    Implement an All and a Get methods

--- internal/tests/mock/mock_stats.go
+++ internal/tests/mock/mock_stats.go
@@ -1,6 +1,10 @@
 package mock
 
-import "github.com/pchchv/go-url-shortener/internal/core/domain"
+import (
+	"context"
+
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
+)
 
 type MockStatsRepo struct {
 	Stats []domain.Stats
@@ -10,4 +14,17 @@ func NewMockStatsRepo() *MockStatsRepo {
 	return &MockStatsRepo{
 		Stats: MockStatsData,
 	}
+}
+
+func (m *MockStatsRepo) All(ctx context.Context) ([]domain.Stats, error) {
+	return m.Stats, nil
+}
+
+func (m *MockStatsRepo) Get(ctx context.Context, id string) (domain.Stats, error) {
+	for _, stats := range m.Stats {
+		if stats.Id == id {
+			return stats, nil
+		}
+	}
+	return domain.Stats{}, nil
 }
