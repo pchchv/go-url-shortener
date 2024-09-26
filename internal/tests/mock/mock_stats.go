ed2e70eb22f59c8a34e3e7a635d4cbebf2f91ccc
pchchv <ipchchv@gmail.com>
2024-09-26 11:07:56 +0300

    Implement Create and Delete methods

--- internal/tests/mock/mock_stats.go
+++ internal/tests/mock/mock_stats.go
@@ -28,3 +28,18 @@ func (m *MockStatsRepo) Get(ctx context.Context, id string) (domain.Stats, error
 	}
 	return domain.Stats{}, nil
 }
+
+func (m *MockStatsRepo) Create(ctx context.Context, stats domain.Stats) error {
+	m.Stats = append(m.Stats, stats)
+	return nil
+}
+
+func (m *MockStatsRepo) Delete(ctx context.Context, id string) error {
+	for i, stats := range m.Stats {
+		if stats.Id == id {
+			m.Stats = append(m.Stats[:i], m.Stats[i+1:]...)
+			break
+		}
+	}
+	return nil
+}
