4aad887375042080899c0f5c33d5fc2a563133a2
pchchv <ipchchv@gmail.com>
2024-09-27 08:28:30 +0300

    Implement an All and a Get methods

--- internal/adapters/repository/stats.go
+++ internal/adapters/repository/stats.go
@@ -2,10 +2,14 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	ddbtypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
 )
 
 type StatsRepository struct {
@@ -24,4 +28,43 @@ func NewStatsRepository(ctx context.Context, tableName string) *StatsRepository 
 		client:    client,
 		tableName: tableName,
 	}
+}
+
+func (d *StatsRepository) All(ctx context.Context) ([]domain.Stats, error) {
+	input := &dynamodb.ScanInput{
+		TableName: &d.tableName,
+	}
+
+	result, err := d.client.Scan(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan table: %w", err)
+	}
+
+	stats := []domain.Stats{}
+	if err = attributevalue.UnmarshalListOfMaps(result.Items, &stats); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal data: %w", err)
+	}
+
+	return stats, nil
+}
+
+func (d *StatsRepository) Get(ctx context.Context, id string) (domain.Stats, error) {
+	input := &dynamodb.GetItemInput{
+		TableName: &d.tableName,
+		Key: map[string]ddbtypes.AttributeValue{
+			"id": &ddbtypes.AttributeValueMemberS{Value: id},
+		},
+	}
+
+	result, err := d.client.GetItem(ctx, input)
+	if err != nil {
+		return domain.Stats{}, fmt.Errorf("failed to get item from DynamoDB: %w", err)
+	}
+
+	stats := domain.Stats{}
+	if err = attributevalue.UnmarshalMap(result.Item, &stats); err != nil {
+		return domain.Stats{}, fmt.Errorf("failed to unmarshal data: %w", err)
+	}
+
+	return stats, nil
 }
