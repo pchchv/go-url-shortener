2694fcd6cbb7277d143b756cb8dcae308788a5cc
pchchv <ipchchv@gmail.com>
2024-09-27 08:47:28 +0300

    Implement a sendMessageToQueue func

--- internal/adapters/handlers/generate.go
+++ internal/adapters/handlers/generate.go
@@ -1,6 +1,17 @@
 package handlers
 
-import "github.com/pchchv/go-url-shortener/internal/core/services"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/pchchv/go-url-shortener/internal/core/domain"
+	"github.com/pchchv/go-url-shortener/internal/core/services"
+)
 
 type RequestBody struct {
 	Long string `json:"long"`
@@ -13,4 +24,27 @@ type GenerateLinkFunctionHandler struct {
 
 func NewGenerateLinkFunctionHandler(l *services.LinkService, s *services.StatsService) *GenerateLinkFunctionHandler {
 	return &GenerateLinkFunctionHandler{linkService: l, statsService: s}
+}
+
+func sendMessageToQueue(ctx context.Context, link domain.Link) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err.Error())
+		return
+	}
+
+	sqsClient := sqs.NewFromConfig(cfg)
+	queueUrl := os.Getenv("QueueUrl")
+	if queueUrl == "" {
+		log.Println("QueueUrl is not set")
+		return
+	}
+
+	_, err = sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    &queueUrl,
+		MessageBody: aws.String("The system generated a short URL with the ID " + link.Id),
+	})
+	if err != nil {
+		fmt.Printf("Failed to send message to SQS, %v", err.Error())
+	}
 }
