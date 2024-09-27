7393adbc0db8dc31a8cf194079f4cbfe5c367f12
pchchv <ipchchv@gmail.com>
2024-09-27 08:45:23 +0300

    Implement a GenerateLinkFunctionHandler

--- internal/adapters/handlers/generate.go
+++ internal/adapters/handlers/generate.go
@@ -1,5 +1,16 @@
 package handlers
 
+import "github.com/pchchv/go-url-shortener/internal/core/services"
+
 type RequestBody struct {
 	Long string `json:"long"`
+}
+
+type GenerateLinkFunctionHandler struct {
+	linkService  *services.LinkService
+	statsService *services.StatsService
+}
+
+func NewGenerateLinkFunctionHandler(l *services.LinkService, s *services.StatsService) *GenerateLinkFunctionHandler {
+	return &GenerateLinkFunctionHandler{linkService: l, statsService: s}
 }
