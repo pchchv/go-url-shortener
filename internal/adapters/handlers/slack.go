6027efa4f245ab725d0d0d6f2aded2863b0c8283
pchchv <ipchchv@gmail.com>
2024-09-27 08:43:40 +0300

    Implement a SlackHandler function

--- internal/adapters/handlers/slack.go
+++ internal/adapters/handlers/slack.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -37,4 +39,26 @@ func HandleAPIGatewayRequest(ctx context.Context, req events.APIGatewayV2HTTPReq
 
 func HandleSQSMessage(ctx context.Context, message events.SQSMessage) error {
 	return PostMessageToSlack(ctx, message.Body)
+}
+
+func SlackHandler(ctx context.Context, event json.RawMessage) error {
+	var sqsEvent events.SQSEvent
+	if err := json.Unmarshal(event, &sqsEvent); err == nil && len(sqsEvent.Records) > 0 {
+		for _, message := range sqsEvent.Records {
+			err := PostMessageToSlack(ctx, message.Body)
+			if err != nil {
+				log.Printf("Error handling SQS message (ID: %s): %v", message.MessageId, err)
+			}
+		}
+		return nil
+	}
+
+	var apiEvent events.APIGatewayV2HTTPRequest
+	log.Print("apiEvent: ", apiEvent)
+	if err := json.Unmarshal(event, &apiEvent); err == nil && apiEvent.RequestContext.HTTP.Method != "" {
+		_, err := HandleAPIGatewayRequest(ctx, apiEvent)
+		return err
+	}
+
+	return fmt.Errorf("invalid event type")
 }
