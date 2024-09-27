da4fcf25ad5036e3be39f518e080aca704e7919e
pchchv <ipchchv@gmail.com>
2024-09-27 08:37:03 +0300

    Implement a Stats method

--- internal/adapters/handlers/stats.go
+++ internal/adapters/handlers/stats.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/pchchv/go-url-shortener/internal/core/services"
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/pchchv/go-url-shortener/internal/core/services"
+)
 
 type StatsFunctionHandler struct {
 	statsService *services.StatsService
@@ -9,4 +16,30 @@ type StatsFunctionHandler struct {
 
 func NewStatsFunctionHandler(l *services.LinkService, s *services.StatsService) *StatsFunctionHandler {
 	return &StatsFunctionHandler{linkService: l, statsService: s}
+}
+
+func (s *StatsFunctionHandler) Stats(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	links, err := s.linkService.GetAll(ctx)
+	if err != nil {
+		return ServerError(err)
+	}
+
+	for i, link := range links {
+		stats, err := s.statsService.GetStatsByLinkID(ctx, link.Id)
+		if err != nil {
+			log.Printf("Error getting stats for link '%s': %v", link.Id, err)
+			continue
+		}
+		links[i].Stats = stats
+	}
+
+	jsonResponse, err := json.Marshal(links)
+	if err != nil {
+		return ServerError(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(jsonResponse),
+	}, nil
 }
