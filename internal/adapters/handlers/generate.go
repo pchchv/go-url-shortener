bb3e5a4c44c7dfb7a3569434a6af9de8515a4cf3
pchchv <ipchchv@gmail.com>
2024-09-27 08:50:19 +0300

    Implement a CreateShortLink method

--- internal/adapters/handlers/generate.go
+++ internal/adapters/handlers/generate.go
@@ -3,14 +3,19 @@ package handlers
 import (
 	"context"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
 	"github.com/pchchv/go-url-shortener/internal/core/domain"
 	"github.com/pchchv/go-url-shortener/internal/core/services"
 )
@@ -26,6 +31,57 @@ type GenerateLinkFunctionHandler struct {
 
 func NewGenerateLinkFunctionHandler(l *services.LinkService, s *services.StatsService) *GenerateLinkFunctionHandler {
 	return &GenerateLinkFunctionHandler{linkService: l, statsService: s}
+}
+
+func (h *GenerateLinkFunctionHandler) CreateShortLink(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	var err error
+	var requestBody RequestBody
+	if err := json.Unmarshal([]byte(req.Body), &requestBody); err != nil {
+		return ClientError(http.StatusBadRequest, "Invalid JSON")
+	}
+
+	if requestBody.Long == "" {
+		return ClientError(http.StatusBadRequest, "URL cannot be empty")
+	}
+
+	if len(requestBody.Long) < 15 {
+		return ClientError(http.StatusBadRequest, "URL must be at least 15 characters long")
+	}
+
+	if !IsValidLink(requestBody.Long) {
+		return ClientError(http.StatusBadRequest, "Invalid URL format")
+	}
+
+	link := domain.Link{
+		Id:          GenerateShortURLID(8),
+		OriginalURL: requestBody.Long,
+		CreatedAt:   time.Now(),
+	}
+
+	if err = h.linkService.Create(ctx, link); err != nil {
+		return ServerError(err)
+	}
+
+	js, err := json.Marshal(link)
+	if err != nil {
+		return ServerError(err)
+	}
+
+	if err = h.statsService.Create(ctx, domain.Stats{
+		Id:        uuid.NewString(),
+		LinkID:    link.Id,
+		Platform:  domain.PlatformTwitter,
+		CreatedAt: time.Now(),
+	}); err != nil {
+		log.Println("failed to create stats: ", err)
+	}
+
+	sendMessageToQueue(ctx, link)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(js),
+	}, nil
 }
 
 func sendMessageToQueue(ctx context.Context, link domain.Link) {
