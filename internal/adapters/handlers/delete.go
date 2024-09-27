9d9f86d108251dacee367d6ed5555ee851ce1c9d
pchchv <ipchchv@gmail.com>
2024-09-27 08:52:03 +0300

    Implement a Delete method

--- internal/adapters/handlers/delete.go
+++ internal/adapters/handlers/delete.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "github.com/pchchv/go-url-shortener/internal/core/services"
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/pchchv/go-url-shortener/internal/core/services"
+)
 
 type DeleteFunctionHandler struct {
 	statsService *services.StatsService
@@ -9,4 +14,18 @@ type DeleteFunctionHandler struct {
 
 func NewDeleteFunctionHandler(l *services.LinkService, s *services.StatsService) *DeleteFunctionHandler {
 	return &DeleteFunctionHandler{linkService: l, statsService: s}
+}
+
+func (s *DeleteFunctionHandler) Delete(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	id := req.PathParameters["id"]
+
+	if err := s.linkService.Delete(ctx, id); err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+
+	if err := s.statsService.Delete(ctx, id); err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+
+	return events.APIGatewayProxyResponse{StatusCode: 204}, nil
 }
