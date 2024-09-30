07ce7b2bdc8c7279dae1e657f2a6d71afb9bc0e5
pchchv <ipchchv@gmail.com>
2024-09-30 09:20:00 +0300

    Implement stats adapter func

---
+++ internal/adapters/functions/stats/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pchchv/go-url-shortener/internal/adapters/cache"
+	"github.com/pchchv/go-url-shortener/internal/adapters/handlers"
+	"github.com/pchchv/go-url-shortener/internal/adapters/repository"
+	"github.com/pchchv/go-url-shortener/internal/config"
+	"github.com/pchchv/go-url-shortener/internal/core/services"
+)
+
+func main() {
+	appConfig := config.NewConfig()
+	redisAddress, redisPassword, redisDB := appConfig.GetRedisParams()
+	linkTableName := appConfig.GetLinkTableName()
+	statsTableName := appConfig.GetStatsTableName()
+
+	cache := cache.NewRedisCache(redisAddress, redisPassword, redisDB)
+
+	linkRepo := repository.NewLinkRepository(context.TODO(), linkTableName)
+	statsRepo := repository.NewStatsRepository(context.TODO(), statsTableName)
+
+	linkService := services.NewLinkService(linkRepo, cache)
+	statsService := services.NewStatsService(statsRepo, cache)
+
+	handler := handlers.NewStatsFunctionHandler(linkService, statsService)
+
+	lambda.Start(handler.Stats)
+}
